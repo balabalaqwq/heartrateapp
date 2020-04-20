1cbd84f6e2d7b52c9af6b7fb348052cddbd40631
wangteng <1255824627@qq.com>
2020-04-20 13:20:04 +0800

    登录+demo

--- app/src/main/java/com/example/heartrateapp/MainActivity.java
+++ app/src/main/java/com/example/heartrateapp/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.heartrateapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button_start = (Button) findViewById(R.id.start_Testing);
+        button_start.setOnClickListener(
+               new View.OnClickListener() {
+                   @Override
+                   public void onClick(View v) {
+                       Intent intent = new Intent(MainActivity.this, HeartRateActivity.class);
+                       startActivity(intent);
+                   }
+               }
+       );
     }
 }
