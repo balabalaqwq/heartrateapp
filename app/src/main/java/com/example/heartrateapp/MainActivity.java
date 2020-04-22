6799825c104159cf3d7350e9a7afc30b8fb94e63
wangteng <1255824627@qq.com>
2020-04-22 09:52:25 +0800

    第三次提交

--- app/src/main/java/com/example/heartrateapp/MainActivity.java
+++ app/src/main/java/com/example/heartrateapp/MainActivity.java
@@ -1,27 +1,83 @@
 package com.example.heartrateapp;
 
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+
+import android.util.SparseArray;
 import android.view.View;
-import android.widget.Button;
+
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private RadioGroup mTabRadioGroup;
+    private SparseArray<Fragment> mFragmentSparseArray;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
 
-        Button button_start = (Button) findViewById(R.id.start_Testing);
-        button_start.setOnClickListener(
-               new View.OnClickListener() {
-                   @Override
-                   public void onClick(View v) {
-                       Intent intent = new Intent(MainActivity.this, HeartRateActivity.class);
-                       startActivity(intent);
-                   }
-               }
-       );
+    private void initView() {
+        mTabRadioGroup = findViewById(R.id.tabs_rg);
+        mFragmentSparseArray = new SparseArray<>();
+        mFragmentSparseArray.append(R.id.today_tab, BlankFragment.newInstance("本APP可仅通过手机摄" +
+                "像头及闪光灯达到测量实时的人体心率数据。" +
+                "注意： 该功能测得的数据仅供参考，因设备、灯光等环境因素影响与实际心率略有偏差。" +
+                "若发现身体不适建议及时就医！"));
+        mFragmentSparseArray.append(R.id.record_tab, BlankFragment.newInstance("记录"));
+        mFragmentSparseArray.append(R.id.contact_tab, BlankFragment.newInstance("丁香园——心血管"));
+        mFragmentSparseArray.append(R.id.settings_tab, BlankFragment.newInstance("设置"));
+
+
+
+        mTabRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                // 具体的fragment切换逻辑可以根据应用调整，例如使用show()/hide()
+//
+//                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+//                        mFragmentSparseArray.get(checkedId)).commit();
+
+
+                switch (checkedId)  {
+                    case R.id.today_tab:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        mFragmentSparseArray.get(checkedId)).commit();
+                    case R.id.record_tab:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                                mFragmentSparseArray.get(checkedId)).commit();
+                    case R.id.contact_tab:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                                mFragmentSparseArray.get(checkedId)).commit();
+
+
+                    case R.id.settings_tab:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                                mFragmentSparseArray.get(checkedId)).commit();
+                    default:
+
+                }
+
+
+            }
+        });
+        // 默认显示第一个
+        getSupportFragmentManager().beginTransaction().add(R.id.fragment_container,
+                mFragmentSparseArray.get(R.id.today_tab)).commit();
+
+        findViewById(R.id.test_now).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, HeartRateActivity.class));
+            }
+        });
     }
 }
